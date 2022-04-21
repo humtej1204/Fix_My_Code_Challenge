43a58a61bf4067099edecc27543245cf2112b937
Humberto Tejada <humtej1204@gmail.com>
2022-04-21 04:28:06 -0500

    Correcting line 43: Changing "_password" to "__pasword" and line 57: Changing "upper" to "lower"

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -40,7 +40,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -50,11 +50,11 @@ class User():
         - `False` if `__password` is `None`
         - Compare `__password` and the MD5 value of `pwd`
         """
-        if pwd is None or type(pwd) is not str:
+        if (pwd is None) or (type(pwd) is not str):
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.__password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 
 
 if __name__ == '__main__':
