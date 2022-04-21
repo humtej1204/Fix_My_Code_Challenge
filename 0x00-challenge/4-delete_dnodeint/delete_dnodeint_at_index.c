9559f00b04cd625b35af2d2dc6d593271855381a
Humberto Tejada <humtej1204@gmail.com>
2022-04-21 04:46:19 -0500

    Correcting line 46: Changing "(*head)->prev->prev = (*head)->prev" to "(*head)->prev->next = (*head)->next"

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,10 +43,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev->prev = (*head)->prev;
+		(*head)->prev->next = (*head)->next;
 		free(*head);
 		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
+			(*head)->next->prev = (*head)->prev;	
 		*head = saved_head;
 	}
 	return (1);
